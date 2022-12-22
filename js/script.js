adb839431b41ef4688151e1b04e258554ce32f6a
Israa Ezzat <62281743+israa639@users.noreply.github.com>
2022-12-22 14:54:04 +0200

    The main project structure

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+
+
+document.getElementById("contact").addEventListener('click',function(){
+    document.getElementById("menu-section").style.visibility="hidden";
+    document.getElementById("contact-section").style.visibility="visible";
+ });
+ document.getElementById("about").addEventListener('click',function(){
+     document.getElementById("menu-section").style.visibility="visible";
+     document.getElementById("contact-section").style.visibility="hidden";
+  });
