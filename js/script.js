b7ebcf88a0b7802f8a495a1d189f9d995c92e733
Israa Ezzat <62281743+israa639@users.noreply.github.com>
2023-01-02 22:09:03 +0200

    final version

--- js/script.js
+++ js/script.js
@@ -2,10 +2,30 @@
 
 
 document.getElementById("contact").addEventListener('click',function(){
-    document.getElementById("menu-section").style.visibility="hidden";
+    
     document.getElementById("contact-section").style.visibility="visible";
+    document.getElementById("about-section").style.visibility="hidden";
+     document.getElementById("offers-section").style.visibility="hidden";
+     document.getElementById("menu-section").style.visibility="hidden";
+    
  });
- document.getElementById("about").addEventListener('click',function(){
+ document.getElementById("menu").addEventListener('click',function(){
      document.getElementById("menu-section").style.visibility="visible";
      document.getElementById("contact-section").style.visibility="hidden";
+     document.getElementById("about-section").style.visibility="hidden";
+     document.getElementById("offers-section").style.visibility="hidden";
   });
+  document.getElementById("offers").addEventListener('click',function(){
+    document.getElementById("menu-section").style.visibility="hidden";
+    document.getElementById("contact-section").style.visibility="hidden";
+    document.getElementById("about-section").style.visibility="hidden";
+    document.getElementById("offers").style.visibility="visible";
+   
+ });
+ document.getElementById("about").addEventListener('click',function(){
+    document.getElementById("menu-section").style.visibility="hidden";
+    document.getElementById("contact-section").style.visibility="hidden";
+    document.getElementById("about-section").style.visibility="visible";
+    document.getElementById("offers-section").style.visibility="hidden";
+ 
+ });
